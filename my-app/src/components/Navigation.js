811df8abce12dac5a91e4b10bf0b4899cfcc7f97
alyStrom5062 <Alyenastrom00@gmail.com>
2024-01-01 16:25:50 -0800

    Began working on app

---
+++ my-app/src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <>
+      <nav>
+        <div className="grid-container">
+          <div className="grid-item">
+            <a href="#">Link 1</a>
+          </div>
+          <div className="grid-item">
+            <a href="#">Link 2</a>
+          </div>
+          <div className="grid-item">
+            <a href="#">Link 3</a>
+          </div>
+          <div className="grid-item">
+            <a href="#">Link 4</a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
